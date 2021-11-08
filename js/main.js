3ef53f56f4d7338fe9b1502a1af4b25823db50f3
Seamus McDonald <pseamus1212@gmail.com>
2021-11-07 20:41:58 -0800

    working scroll animations

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /* Animations */
 
-var textElements = document.querySelectorAll('.animatedText');
-
+var scrollRevealElements = document.querySelectorAll('.scrollReveal');
 var scrollDown = document.querySelectorAll('#scrollDown');
+
 
 var scrollHoverHop = anime({
     targets: scrollDown,
@@ -11,3 +11,28 @@ var scrollHoverHop = anime({
     easing: 'easeOutQuad',
     loop: true,
 })
+
+const handleScrollReveal = () => {
+
+    const scrollOffset = (window.innerHeight) * .10
+
+    const isInView = (element) => {
+        elementDistance = element.getBoundingClientRect().top
+        return elementDistance <= (window.innerHeight - scrollOffset)
+    }
+
+    scrollRevealElements.forEach((textElement) => {
+        if (isInView(textElement)) {
+            textElement.classList.add('display')
+        }
+    })
+}
+
+window.onload = () => {
+    handleScrollReveal();
+};
+
+window.addEventListener('scroll', () => {
+    handleScrollReveal();
+})
+   
