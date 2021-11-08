1534e8f56e491f15aa72c088b9698c8602b87016
Seamus McDonald <pseamus1212@gmail.com>
2021-11-07 22:12:17 -0800

    animation update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 /* Animations */
 
 var scrollRevealElements = document.querySelectorAll('.scrollReveal');
-var scrollDown = document.querySelectorAll('#scrollDown');
+var scrollDown = document.querySelector('#scrollDown');
+var arrowSvg = document.querySelector('#contactArrowPath')
+var arrowHeadPath = document.querySelector('#contactArrowHead > path')
 
+const scrollOffset = (window.innerHeight) * .10
+var arrowPlayed = false;
+
+const isInView = (element) => {
+    elementDistance = element.getBoundingClientRect().top
+    return elementDistance <= (window.innerHeight - scrollOffset)
+}
 
 var scrollHoverHop = anime({
     targets: scrollDown,
@@ -12,20 +21,42 @@ var scrollHoverHop = anime({
     loop: true,
 })
 
-const handleScrollReveal = () => {
+var arrowHeadDraw = anime({
+    targets: arrowHeadPath,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 600,
+    loop: false,
+    autoplay: false,
+    direction: 'alternate',
+})
 
-    const scrollOffset = (window.innerHeight) * .10
-
-    const isInView = (element) => {
-        elementDistance = element.getBoundingClientRect().top
-        return elementDistance <= (window.innerHeight - scrollOffset)
+var arrowDrawPath = anime({
+    targets: arrowSvg,
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2000,
+    loop: false,
+    autoplay: false,
+    direction: 'alternate',
+    complete: function(anim) {
+        anim.complete ? arrowHeadDraw.play() : null
     }
+})
+
+const handleScrollReveal = () => {
 
     scrollRevealElements.forEach((textElement) => {
         if (isInView(textElement)) {
-            textElement.classList.add('display')
+            textElement.classList.add('display');
         }
     })
+
+    if(isInView(arrowSvg) && !arrowPlayed) {
+        console.log(arrowHeadPath)
+        arrowDrawPath.play();
+        arrowPlayed = true;
+    }
 }
 
 window.onload = () => {
